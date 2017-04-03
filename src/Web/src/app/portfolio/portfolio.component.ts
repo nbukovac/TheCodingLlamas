1d3e09225239bc55087eeacdad9cf9dbda30b709
NMilicic <nikola.mlicic991@gmal.com>
2017-04-03 20:52:46 +0200

    Some style changes

--- src/Web/src/app/portfolio/portfolio.component.ts
+++ src/Web/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-
-  constructor() { }
+projects: Project;
+  constructor(private http : Http) {
+    http.get('http://localhost:14924/api/projects').subscribe(result => {
+          this.projects = result.json();
+        });
+   }
 
   ngOnInit() {
   }
 
+}
+
+export class Project {
+ Name: string;
+ Description: string;
+ Year: number;
+ PictureUrl: string;
+ LiveDemoUrl: string;
+ GithubUrl: string;
 }
